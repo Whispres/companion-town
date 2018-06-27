76a730129a6101509ff691d43dba8c6e4fbb37e8
Andre Correia <andre.correia@farfetch.com>
2018-06-27 18:29:14 +0100

    add base repository and repository for users

---
+++ src/CompanionTown/Api/Repositories/BaseRepository.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Driver;
+
+namespace Api.Repositories
+{
+    public abstract class BaseRepository<T>
+    {
+        private readonly IMongoDatabase _mongoDatabase;
+
+        public readonly string _collectionName;
+
+        private IMongoCollection<T> Collection;
+
+        public BaseRepository(IMongoDatabase mongoDatabase, string collectionName)
+        {
+            this._mongoDatabase = mongoDatabase;
+            this._collectionName = collectionName;
+        }
+
+        protected IMongoCollection<T> GetCollection()
+        {
+            if (this.Collection == null)
+            {
+                this.Collection = _mongoDatabase.GetCollection<T>(this._collectionName);
+            }
+
+            return this.Collection;
+        }
+    }
+}
---
+++ src/CompanionTown/Api/Repositories/UserRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Api.Models;
+using MongoDB.Driver;
+
+namespace Api.Repositories
+{
+    public class UserRepository : BaseRepository<User>
+    {
+        // hack : magic string to be removed to config !?!?!
+        public UserRepository(IMongoDatabase mongoDatabase) : base(mongoDatabase, "Users")
+        {
+        }
+
+        public async Task<bool> InsertAsync(User user)
+        {
+            try
+            {
+                await this.GetCollection().InsertOneAsync(user).ConfigureAwait(false);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
