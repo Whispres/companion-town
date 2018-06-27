5514569c361775882cab60a53eef1b65af9b333b
Andre Correia <andre.correia@farfetch.com>
2018-06-27 18:36:50 +0100

    add user controller (not fully implemented)

---
+++ src/CompanionTown/Api/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        // POST api/user
+        [HttpPost]
+        public void Post([FromBody] User user)
+        {
+        }
+    }
+}
