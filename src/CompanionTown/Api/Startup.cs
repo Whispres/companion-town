873cbcc4ed38f6ba143700034975c19635c4f25a
Andre Correia <andre.correia@farfetch.com>
2018-06-27 18:36:57 +0100

    add swagger

--- src/CompanionTown/Api/Startup.cs
+++ src/CompanionTown/Api/Startup.cs
@@ -10,11 +10,14 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Api
 {
     public class Startup
     {
+        private readonly string SwaggerApiTitle = "Companion Town API";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -25,6 +28,11 @@ namespace Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = SwaggerApiTitle, Version = "v1" });
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -39,8 +47,15 @@ namespace Api
             {
                 app.UseHsts();
             }
+            
+            app.UseSwagger();
 
-            app.UseHttpsRedirection();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", SwaggerApiTitle);
+            });
+
+            //app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
